d886b9ce6ddd34fb985bbc0739f0b473d759cd28
Joshua <fleshmanjoshuac@hotmail.com>
2022-01-29 15:24:02 -0600

    Initial Commit

--- main.py
+++ main.py
@@ -1,17 +1,46 @@
-# This is a sample Python script.
+# Importing flask module in the project is mandatory
+# An object of Flask class is our WSGI application.
+from flask import Flask, redirect, url_for, request
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# Flask constructor takes the name of 
+# current module (__name__) as argument.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
+# The route() function of the Flask class is a decorator,
+# which tells the application which URL should call 
+# the associated function.
+@app.route('/')
+# ‘/’ URL is bound with hello_world() function.
+def hello_world():
+    return 'Hello World'
+
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s!' % name
+
+@app.route('/fu')
+def fu():
+    return "FU"
+
+
+@app.route('/success/<name>')
+def success(name):
+    return 'welcome %s' % name
+
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
+
+
+# main driver function
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+    # run() method of Flask class runs the application
+    # on the local development server.
+    app.run()
