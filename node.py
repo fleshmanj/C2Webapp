a269d3f0c6e050b54ecb4fdb456a454886315fe9
Joshua <fleshmanjoshuac@hotmail.com>
2022-01-31 20:42:09 -0600

    Removed Node class and added server uptime function

--- node.py
+++ node.py
@@ -1,12 +1,18 @@
 # Importing flask module in the project is mandatory
 # An object of Flask class is our WSGI application.
-from flask import Flask, redirect, url_for, request, render_template
-from flask.views import MethodView
+import datetime
+import time
+
+from flask import Flask, redirect, url_for, request, render_template, Response
+
+
+
 
 # Flask constructor takes the name of 
 # current module (__name__) as argument.
 app = Flask(__name__)
 
+C2_ADDRESS = None
 
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call 
@@ -22,16 +28,6 @@ def hello_name(name):
     return 'Hello %s!' % name
 
 
-@app.route('/fu')
-def fu():
-    return "FU"
-
-
-@app.route('/success/<name>')
-def success(name):
-    return 'welcome %s' % name
-
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
@@ -40,28 +36,39 @@ def login():
     else:
         return render_template('login.html')
 
+@app.route('/id')
+def show_id():
+    return
 
-class Node(object):
+@app.route('/set_id/<id>')
+def set_id(id):
+    return 'welcome %s' % id
 
-    def __init__(self, C2_address="0.0.0.0", port=80):
-        self.id = None
-        self.port = port
-        self.C2_address = C2_address
-        print("Init called")
 
-    @app.route('/id')
-    def show_id(self):
-        return self.id
+@app.route('/uptime', methods=['GET'])
+def uptime():
+    global start
+    sdt = datetime.datetime.fromtimestamp(start)
+    ndt = datetime.datetime.now()
+    if request.method == 'GET':
+        return f"Server runtime is {ndt - sdt}."
 
-    @app.route('/set_id/<id>')
-    def set_id(self, id):
-        self.id = id
-        return 'welcome %s' % id
+
+@app.route('/set_C2_address/<address>', methods=['POST'])
+def set_C2_address(address):
+    global C2_ADDRESS
+    if C2_ADDRESS is None:
+        C2_ADDRESS = address
+        return Response(status=200)
+    else:
+        return Response(status=403)
+
+
 
 
 # main driver function
 if __name__ == '__main__':
     # run() method of Flask class runs the application
     # on the local development server.
-    client = Node()
+    start = time.time()
     app.run(debug=True, host="0.0.0.0", port=80)
